d5952f2cff4f19acbd56027b1e82c6d6b26e3180
Tarcísio Júnior <tarcisiosouzajrr@gmail.com>
2021-05-07 17:57:47 -0300

    PHP e banco
    Adição de arquivos .php para conexão com o banco de dados e alterações simples dos arquivos .html

---
+++ codigo/php/cadastro.php
@@ -0,0 +1,32 @@
+<?php
+$username = filter_input(INPUT_POST, 'username');
+$email = filter_input(INPUT_POST, 'email');
+$gender = filter_input(INPUT_POST, 'gender');
+$birth = filter_input(INPUT_POST, 'birth');
+$cpf = filter_input(INPUT_POST, 'cpf');
+$cnh = filter_input(INPUT_POST, 'cnh');
+$senha = filter_input(INPUT_POST, 'password');
+
+if($gender == 'masculino'){
+    $gender = 'M';
+} else if($gender == 'feminino'){
+    $gender = 'F';
+}
+
+if (!empty($username)){
+    $host = "";
+    $database = "";
+    $user = "";
+    $password = "";
+    
+    $connection = pg_connect("host=$host dbname=$database user=$user password=$password") or die("Erro de conexao<br/>");
+    $query = "INSERT INTO cliente (cpf, cnh, sexo, dtnascimento, nome, email, senha) VALUES ('$cpf', '$cnh', '$gender', '$birth', '$username', '$email', '$senha')";
+    $result_set = pg_query($connection, $query); 
+
+    pg_free_result($result_set);
+    pg_close($connection);
+    
+    header('Location: ../html/login.html');
+    exit();
+}
+?>
---
+++ codigo/php/login.php
@@ -0,0 +1,33 @@
+<?php
+    $campoEmail = filter_input(INPUT_GET, 'email');
+    $campoSenha = filter_input(INPUT_GET, 'password');
+    $host = "";
+    $database = "";
+    $user = "";
+    $password = "";
+    $connection = pg_connect("host=$host dbname=$database user=$user password=$password");
+    $query = "SELECT * from cliente";
+    $result_set = pg_query($connection, $query) or die("Erro de conexao! <br/>");
+
+    $loginCorreto = false;
+    
+    while ($row = pg_fetch_row($result_set))
+    {
+        $email = $row[5];
+        $senha = $row[7];
+        if($senha == $campoSenha && $email == $campoEmail){
+            $loginCorreto = true;
+        }
+    }
+
+    pg_free_result($result_set);
+    pg_close($connection);
+    
+    if($loginCorreto){
+        header('Location: ../html/home.html');
+        exit();
+    } else {
+        header('Location: ../html/login.html');
+        exit();
+    }
+?>
