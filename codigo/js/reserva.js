c2bb3c54d9cf049b74fc3794cdd7811e672ce852
Paulo Victor <66500604+paulobh199@users.noreply.github.com>
2021-06-03 14:28:25 -0300

    Update reserva.js

--- codigo/js/reserva.js
+++ codigo/js/reserva.js
@@ -76,10 +76,59 @@ confirmar=()=>{
         `
             <div class="fimTitulo" id="fim">
                 <label for="">
-                    <p id="tituloEtapa1" >Obrigado!</p> 
-                    <a id="voltarLink" class="nav-link active" href="../html/home.html"> Voltar para página principal</a>
+                    <p id="tituloEtapa1" >Obrigado!</p>
+
+                    <div class="avaliacao">
+                        <p>Nos ajude a melhorar sua experiência. Dê uma nota de 1 a 5, para avaliar nosso sistema.</p>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="1">
+                            <label class="form-check-label" for="inlineRadio1">1</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="2">
+                            <label class="form-check-label" for="inlineRadio2">2</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="3">
+                            <label class="form-check-label" for="inlineRadio2">3</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4" value="4">
+                            <label class="form-check-label" for="inlineRadio2">4</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio5" value="5">
+                            <label class="form-check-label" for="inlineRadio2">5</label>
+                        </div>
+                    </div>
+
+                    <a id="voltarLink" class="nav-link active" href="#"> Voltar para página principal</a>
+
                 </label>
+                
+                
             </div>
+            
         `;
+        voltarHome();
     }
 }
+
+voltarHome=()=>{
+    let voltar = document.getElementById('voltarLink');
+        function alerta() {alert("Selecione uma nota.")};
+        voltar.onclick=()=>{
+            
+            let valor = document.querySelector('input[name="inlineRadioOptions"]:checked');
+            
+            if(valor != null){
+                console.log(valor.value);
+                window.location.href = "http://localhost/codigo/html/home.html"
+            }else{
+                alerta();
+            }
+            
+        
+        }
+}
+
